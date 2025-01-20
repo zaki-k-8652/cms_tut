5fef5b730f385590fd1cbe77097f96d695a1b392
zaki-k-8652 <khanzaki8652@gmail.com>
2025-01-21 00:23:44 +0530

    Major changes: CSS layout added and page locally hosted

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-
+const { engine } = require('express-handlebars');
+const {mongoDbUrl} = require('./config/configuration');
 const app = express();
 
-// Configure Mongoose to MongoDB
-mongoose.connect('mongodb://localhost:27017/cms_tut')
+mongoose.connect(mongoDbUrl)
     .then(response => {
         console.log("MongoDB Connected!");
-    })
-    .catch(err => {
+    }).catch(err => {
         console.log("MongoDB connection failed!", err);
     });
 
-
-// Configure Express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
-app.use('/', (req, res) => {
-    res.send("Welcome to MMANTC cms")
+app.engine('handlebars', engine({ defaultLayout: 'default' }));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    console.log("Rendering index page...");
+    res.render('default/index');
 });
 
-app.listen(3000,() =>{
+app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
